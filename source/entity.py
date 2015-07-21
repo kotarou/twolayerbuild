5be9796f40262759eb373b488b6e8d6fdc9c9492
Kieran Carnegie <kotarou@ecs.vuw.ac.nz>
2015-07-21 22:43:35 +1200

    Entity render example added

--- source/entity.py
+++ source/entity.py
@@ -3,6 +3,7 @@
 @author: Kotarou
 """
 from abc import ABCMeta, abstractmethod
+import pyglet 
 
 class Entity:
     __metaclass__ = ABCMeta
@@ -15,30 +16,59 @@ class Entity:
         self.identifier = identifier
         
     @abstractmethod
-    def respond(self):
-        print("Ouch")
+    def respond(self): pass
+
+    @abstractmethod
+    def draw(self): pass
         
 class Actor(Entity):
+    sprite = "" 
+    
     name = ""
     description = ""
     identifier = ""
+    
+    posX = 0
+    posY = 0
+    
+    alive = True    
     
     def __init__(self, identifier):
         self.identifier = identifier
         
     def respond(self):
         print("Owie")
-        
+    
+    def updatePosition(self, dx, dy):
+        self.x += dx
+        self.y += dy
+    
+    def setPosition(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def kill(self):
+        self.alive = False
+                
+    def draw(self):
+        pyglet.graphics.draw_indexed(4, pyglet.gl.GL_TRIANGLES, [0, 1, 2, 0, 2, 3], ('v2i', (100, 100,150, 100, 150, 150, 100, 150)))        
+
 class Trap(Entity):
     name = ""
     description = ""
     identifier = ""
     
+    location = ""
+        
     def __init__(self, identifier):
         self.identifier = identifier
         
     def respond(self):
         print("I am a trap")
+        
+    def activate(self, target):
+        print("BANG")
+            
         
 class Object(Entity):
     name = ""
--- source/game.py
+++ source/game.py
@@ -12,4 +12,15 @@ y.respond()
 
 game_window = pyglet.window.Window(800, 600) 
 
+
+
+@game_window.event
+def on_draw(): 
+    print("hi")
+    game_window.clear() 
+    x.draw()    
+    #level_label.draw() 
+    #score_label.draw()
+    
+    
 pyglet.app.run()
