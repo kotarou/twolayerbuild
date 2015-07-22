5fb08ed967a01e7d19e442655c0970219e79a468
Kieran Carnegie <kotarou@ecs.vuw.ac.nz>
2015-07-22 16:42:45 +1200

    Method descriptions and actor positional code

--- source/entity.py
+++ source/entity.py
@@ -3,13 +3,20 @@
 @author: Kotarou
 """
 from abc import ABCMeta, abstractmethod
+import numpy as np
 import pyglet 
 
-class Entity:  
+class Entity: 
+    """
+        The base class for any object inside the game.
+    """ 
     name = ""
     description = ""
     identifier = ""
-        
+    
+    renderable = True
+    interactable = True
+
     @abstractmethod
     def respond(self): pass
 
@@ -18,40 +25,51 @@ class Entity:
 
     @abstractmethod
     def update(self, dt): pass
+
+    def toString(self):
+        return identifier    
         
 class Actor(Entity):
+    """
+        Base class for any moving actor within the game.
+        The assumption is that any moving actor can die.
+    """
     sprite = "" 
-    
-    name = ""
-    description = ""
-    identifier = ""
+    pos = np.array([0.0,0.0])
+    vel = np.array([0.0,0.0])
+    alive = True
+    description = "A moving actor of unknown particulars"
 
     def __init__(self, identifier, x, y):
         self.identifier = identifier
-        self.x = x
-        self.y = y
-
-    x = 0
-    y = 0
-    
-    alive = True    
-           
+        self.pos[0] = x
+        self.pos[1] = y
+       
     def respond(self):
         print("Owie")
     
+    def update(self, dt):
+        self.pos += dt*self.vel
+
+    def setVelocity(self, vx, vy):
+        self.vel[0] = vx
+        self.vel[1] = vy
+
     def updatePosition(self, dx, dy):
-        self.x += dx
-        self.y += dy
+        self.pos[0] += dx
+        self.pos[1] += dy
     
     def setPosition(self, x, y):
-        self.x = x
-        self.y = y
+        self.pos[0] = x
+        self.pos[1] = y
     
     def kill(self):
         self.alive = False
                 
     def draw(self):
-        pyglet.graphics.draw_indexed(4, pyglet.gl.GL_TRIANGLES, [0, 1, 2, 0, 2, 3], ('v2i', (self.x, self.y,self.x+50, self.y, self.x+50, self.y+50, self.x, self.y+50)))        
+        x = int(self.pos[0])
+        y = int(self.pos[1])
+        pyglet.graphics.draw_indexed(4, pyglet.gl.GL_TRIANGLES, [0, 1, 2, 0, 2, 3], ('v2i', (x, y,x+50, y, x+50, y+50, x, y+50)))        
 
 class Trap(Entity):
     name = ""
--- source/game.py
+++ source/game.py
@@ -8,6 +8,7 @@ pyglet.resource.reindex()
 
 xx = entity.Actor(identifier="bob",x=100,y=100)
 xx.respond()
+xx.setVelocity(1,1)
 y = entity.Trap("james")
 y.respond()
 
@@ -19,36 +20,69 @@ interface = [y]
 
 @game_window.event
 def on_draw(): 
+	"""
+		Main game draw method: redraw the game window whenever called.
+	"""
 	# Clear the game window to a black background
 	game_window.clear() 
 
 	# Render all objects that need rendering
 	for ent in renderables:
 		ent.draw()
-    
-	# Render the interface
-	for ent in interface:
-		ent.draw()
+	
+	# Render menus
+
+	# Render interface
+	draw_fps()
 
 	# Console debugging
 	# print("Window redrawn")
-    
-    #level_label.draw() 
-    #score_label.draw()
+	
+	#level_label.draw() 
+	#score_label.draw()
 
 def update(dt): 
+	""" 
+		Game update method. Is called every tick 
+		dt			:	(float)	Time since previous update call
+	"""
 	for ent in renderables: 
 		ent.update(dt)
   
 
 @game_window.event
 def on_mouse_press(x, y, button, modifiers):
+	"""
+		Event that fires whenever a mouse button is clicked.
+		x, y 		:	(int)	The coordinates of the mouse click location
+		button		:	(int)	The mouse button that was pressed. 1 = left click, 4 = right click
+		modifiers	:	(int)	Modifying keys that were pressed in conjunction with the mouse
+	"""
 	print("Mouse clicked")
 	xx.setPosition(x,y)
+	print(xx.renderable)
 
 @game_window.event
 def on_key_press(symbol, modifiers):
-    print(symbol, ' was pressed')
+	"""
+		Event that fires whenever a key is pressed.
+		symbol		:	(int) The key that was pressed. Use key.A / key.LEFT / etc for comparison
+							See pyglet.window.key for a list of keys
+		modifiers	:	(int) Modifying keys that were pressed
+	"""
+	print(symbol, ' was pressed')
+
+def draw_fps():
+	""" 
+		Draw the fps counter in the bottom right of the screen.
+	"""
+		
+	label = pyglet.text.Label("fps: %4.2f" % pyglet.clock.get_fps(),
+						font_name='Times New Roman',
+						font_size=24,
+						x=game_window.width-100, y=70,
+						anchor_x='center', anchor_y='center')
+	label.draw()
 
 pyglet.clock.schedule_interval(update, 1/GAME_TICKS_PER_SECOND) 
 pyglet.app.run()
